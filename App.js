47e1a8e90d16709ad6fd767606a4e973ffa2040a
bhuwan-darai <dr.darai29@gmail.com>
2024-03-19 22:58:39 +0545

    Web view

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from "react-native-webview";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
+  return loading ? (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./assets/inprologo.jpg")}
+        style={styles.loadingImage}
+      />
+      <Image
+        source={require("./assets/loading.gif")}
+        style={styles.loadingImage}
+      />
     </View>
+  ) : (
+    <WebView
+      style={{ marginTop: 34 }}
+      source={{ uri: "Inprostore.vercel.app" }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loadingImage: {
+    width: 100,
+    height: 100,
   },
 });
